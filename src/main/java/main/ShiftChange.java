9ee5c1744cc93cbcc9aa7a6235282b6d7a7ce765
Zack Beach <z.beach@icloud.com>
2020-06-07 17:53:08 -0400

    Expand ShiftChange class

--- src/main/java/main/ShiftChange.java
+++ src/main/java/main/ShiftChange.java
@@ -1,20 +1,37 @@
 package main;
 
 public class ShiftChange implements Comparable {
-  /** Hour of the shift change */
-  public int hour;
+  /** Start time of the shift change */
+  public TimePoint startTime;
+  /** End time of the shift change */
+  public TimePoint endTime;
   /** ID of the shift change */
   public ShiftChangeID id;
 
   /**
    * Constructs a ShiftChange object
    *
-   * @param hour The hour of the shift change
+   * @param startTime The start time of the shift change
+   * @param endTime The end time of the shift change
    * @param id The ShiftChangeID to assign to the shift change
    */
-  public ShiftChange(int hour, ShiftChangeID id) {
-    this.hour = hour;
+  public ShiftChange(TimePoint startTime, TimePoint endTime, ShiftChangeID id) {
+    this.startTime = startTime;
+    this.endTime = endTime;
     this.id = id;
+  }
+
+  /**
+   * Constructs a ShiftChange object
+   *
+   * @param startHour The start hour of the shift change
+   * @param startMinute The start minute of the shift change
+   * @param endHour The end hour of the shift change
+   * @param endMinute The end minute of the shift change
+   * @param id The ShiftChangeID to assign to the shift change
+   */
+  public ShiftChange(int startHour, int startMinute, int endHour, int endMinute, ShiftChangeID id) {
+    this(new TimePoint(startHour, startMinute), new TimePoint(endHour, endMinute), id);
   }
 
   /**
@@ -24,25 +41,30 @@ public class ShiftChange implements Comparable {
    * @param id The ShiftChangeID to assign to the shift change
    */
   public ShiftChange(TimePoint time, ShiftChangeID id) {
-    this.hour = time.hour;
-    this.id = id;
+    this(time, new TimePoint(time.hour, 50), id);
+  }
+
+  /**
+   * Constructs a ShiftChange object
+   *
+   * @param hour The hour of the shift change
+   * @param id The ShiftChangeID to assign to the shift change
+   */
+  public ShiftChange(int hour, ShiftChangeID id) {
+    this(new TimePoint(hour, 10), id);
   }
 
   /** {@inheritDoc} */
   @Override
   public String toString() {
-    return new TimePoint(this.hour, 10).toString()
-        + " - "
-        + new TimePoint(this.hour + 1, 0).toString();
+    return startTime.toString() + " - " + endTime.toString();
   }
 
   /** {@inheritDoc} */
   @Override
   public int compareTo(Object other) {
     if (other instanceof ShiftChange)
-      return (this.hour < ((ShiftChange) other).hour
-          ? -1
-          : this.hour == ((ShiftChange) other).hour ? 0 : 1);
+      return this.startTime.compareTo(((ShiftChange) other).startTime);
     return -2;
   }
 
@@ -50,7 +72,10 @@ public class ShiftChange implements Comparable {
   @Override
   public boolean equals(Object other) {
     if (other instanceof ShiftChange)
-      return this.hour == ((ShiftChange) other).hour && this.id == ((ShiftChange) other).id;
+      return this.startTime.equals(((ShiftChange) other).startTime)
+          && this.endTime.equals(((ShiftChange) other).endTime)
+          && this.id == ((ShiftChange) other).id
+      ;
     return false;
   }
 }
